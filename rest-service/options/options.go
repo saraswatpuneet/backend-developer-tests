23871b0e130700e8e592355be6c7a15dc9a7a098
Puneet Saraswat <61435908+saraswatpuneet@users.noreply.github.com>
2021-11-20 13:26:22 -0600

    set port as env variable

--- rest-service/options/options.go
+++ rest-service/options/options.go
@@ -3,14 +3,15 @@ package options
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 // Options .. contains global options like ones read from environment variables
 type Options struct {
-	Debug          bool  `json:"debug,omitempty"`
-	MaxPayloadSize int64 `json:"max_payload_size,omitempty"`
-	MaxHeaderSize  int   `json:"max_header_size,omitempty"`
-	Port           string   `json:"port,omitempty"`
+	Debug          bool   `json:"debug,omitempty"`
+	MaxPayloadSize int64  `json:"max_payload_size,omitempty"`
+	MaxHeaderSize  int    `json:"max_header_size,omitempty"`
+	Port           string `json:"port,omitempty"`
 }
 
 // New .. create a new instance
@@ -23,6 +24,9 @@ func InitOptions() (*Options, error) {
 	options := New()
 	if err := json.Unmarshal(Default, options); err != nil {
 		return nil, fmt.Errorf("Options initialization unmarshal error: %v", err)
+	}
+	if !options.Debug {
+		options.Port = os.Getenv("PORT")
 	}
 	return options, nil
 }
