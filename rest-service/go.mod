ddb93a87a3992d5d97ae965c29a17c336ccbee31
Puneet Saraswat <61435908+saraswatpuneet@users.noreply.github.com>
2021-11-20 13:12:57 -0600

    Add base stuff for apis: graceful server, handlers, etc.

--- rest-service/go.mod
+++ rest-service/go.mod
@@ -3,5 +3,10 @@ module github.com/stackpath/backend-developer-tests/rest-service
 go 1.15
 
 require (
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.7.4
 	github.com/satori/go.uuid v1.2.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.0
+	gopkg.in/tylerb/graceful.v1 v1.2.15
 )
--- rest-service/main.go
+++ rest-service/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stackpath/backend-developer-tests/rest-service/global"
+	coreserver "github.com/stackpath/backend-developer-tests/rest-service/pkg/core"
 )
 
 func main() {
 	fmt.Println("SP// Backend Developer Test - RESTful Service")
 	fmt.Println()
+	ctx, cancel := context.WithCancel(context.Background())
+	// Only log the warning severity or above.
+	if global.Options.Debug {
+		log.SetLevel(log.DebugLevel)
+	}
 
-	// TODO: Add RESTful web service here
+	// Initialize Rest APIs and start graceful server
+	err := coreserver.CoreServer(ctx, cancel)
+	if err != nil {
+		//send signal to all channels to calm down we found an error
+		log.Errorf("Backend server went crazy: %v", err.Error())
+		// send OS signal and shut it all down
+		os.Exit(1)
+	}
+	log.Infof("Backend intialization completed")
 }
