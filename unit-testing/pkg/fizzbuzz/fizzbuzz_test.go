b1a7028b585dc5435e15125a66981583e2a5ea8d
Puneet Saraswat <61435908+saraswatpuneet@users.noreply.github.com>
2021-11-22 08:03:26 -0600

    add more negative test

--- unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
+++ unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
@@ -194,6 +194,139 @@ func TestFizzBuzz_same_values(t *testing.T) {
 	}
 }
 
+// Test FizzBuzz with zero total number
+func TestFizzBuzz_zero_total(t *testing.T) {
+	tests := []struct {
+		name         string
+		fizzMultiple int64
+		buzzMultiple int64
+		totalNumber  int64
+		expected     []string
+	}{
+		{
+			name:         "zero case",
+			fizzMultiple: 5,
+			buzzMultiple: 3,
+			totalNumber:  0,
+			expected:     []string{},
+		},
+	}
+	// Run the tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := assertPanicFizzBuzzFunction(t, FizzBuzz, test)
+			if len(actual) != len(test.expected) {
+				t.Errorf("Expected %d, got %d", len(test.expected), len(actual))
+			}
+			for i, v := range actual {
+				if v != test.expected[i] {
+					t.Errorf("Expected %s, got %s", test.expected[i], v)
+				}
+			}
+		})
+	}
+}
+
+// Test FizzBuzz with zero fizz multiple
+func TestFizzBuzz_zero_fizzMultiple(t *testing.T) {
+	tests := []struct {
+		name         string
+		fizzMultiple int64
+		buzzMultiple int64
+		totalNumber  int64
+		expected     []string
+	}{
+		{
+			name:         "zero case fizz multiple",
+			fizzMultiple: 0,
+			buzzMultiple: 3,
+			totalNumber:  20,
+			expected:     []string{},
+		},
+	}
+	// Run the tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := assertPanicFizzBuzzFunction(t, FizzBuzz, test)
+			if len(actual) != len(test.expected) {
+				t.Errorf("Expected %d, got %d", len(test.expected), len(actual))
+			}
+			for i, v := range actual {
+				if v != test.expected[i] {
+					t.Errorf("Expected %s, got %s", test.expected[i], v)
+				}
+			}
+		})
+	}
+}
+
+// Test FizzBuzz with zero fizz multiple
+func TestFizzBuzz_zero_buzzMultiple(t *testing.T) {
+	tests := []struct {
+		name         string
+		fizzMultiple int64
+		buzzMultiple int64
+		totalNumber  int64
+		expected     []string
+	}{
+		{
+			name:         "zero case buzz multiple",
+			fizzMultiple: 3,
+			buzzMultiple: 0,
+			totalNumber:  20,
+			expected:     []string{},
+		},
+	}
+	// Run the tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := assertPanicFizzBuzzFunction(t, FizzBuzz, test)
+			if len(actual) != len(test.expected) {
+				t.Errorf("Expected %d, got %d", len(test.expected), len(actual))
+			}
+			for i, v := range actual {
+				if v != test.expected[i] {
+					t.Errorf("Expected %s, got %s", test.expected[i], v)
+				}
+			}
+		})
+	}
+}
+
+
+// Test FizzBuzz with zero fizz multiple
+func TestFizzBuzz_zero_fizzbuzzMultiple(t *testing.T) {
+	tests := []struct {
+		name         string
+		fizzMultiple int64
+		buzzMultiple int64
+		totalNumber  int64
+		expected     []string
+	}{
+		{
+			name:         "zero case fizz and buzz multiple",
+			fizzMultiple: 0,
+			buzzMultiple: 0,
+			totalNumber:  20,
+			expected:     []string{},
+		},
+	}
+	// Run the tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := assertPanicFizzBuzzFunction(t, FizzBuzz, test)
+			if len(actual) != len(test.expected) {
+				t.Errorf("Expected %d, got %d", len(test.expected), len(actual))
+			}
+			for i, v := range actual {
+				if v != test.expected[i] {
+					t.Errorf("Expected %s, got %s", test.expected[i], v)
+				}
+			}
+		})
+	}
+}
+
 // Test FizzBuzz with a negative number
 func TestFizzBuzz_negative_totalNumber(t *testing.T) {
 	tests := []struct {
